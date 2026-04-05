3665a9d1114f43a6e04488669b683e617ab325e3
Ryan Thurber <thurberrt@hotmail.com>
2026-04-05 11:22:53 -0700

    remove restrict keyword from rebound.h, include header guards for c/c++ mixed compilation

--- src/gravity_opt.h
+++ src/gravity_opt.h
@@ -1,9 +1,17 @@
 #ifndef REBOUND_SRC_GRAVITY_OPT_H
 #define REBOUND_SRC_GRAVITY_OPT_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "rebound.h"
 
 void launch_gravity_basic_naive(int N_real, int N_active, double G, double softening2, unsigned int gravity_ignore_terms,
                                     reb_vec6d *gb, reb_particle *particles);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // REBOUND_SRC_GRAVITY_OPT_H
